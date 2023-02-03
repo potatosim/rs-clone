f7497f9209691fb9a72021e8e42e760e9607422e
Grinmak <grinmak@gmail.com>
2023-02-03 23:16:17 +0300

    feat: wrap routes into main route that holds header, footer and Outlet

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,13 +1,16 @@
-import NotFoundPage from 'pages/NotFoundPage/notFoundPage';
+import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AppRoutes } from 'enum/AppRoutes';
+import Layout from 'layout/Layout';
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<div>Hello, World</div>} />
-      <Route path={AppRoutes.NotFoundPage} element={<NotFoundPage />} />
+      <Route path={AppRoutes.Home} element={<Layout />}>
+        <Route index element={<div>Hello, World</div>} />
+        <Route path={AppRoutes.NotFoundPage} element={<NotFoundPage />} />
+      </Route>
     </Routes>
   );
 };
