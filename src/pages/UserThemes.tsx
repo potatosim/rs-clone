8a67aa0825fcb480e122a4004c507753fcf1579c
Leontiy Knyazev <leontiy.knyazev@gmail.com>
2023-02-06 14:27:45 +0600

    feat: add ThemeCreator and ThemeThumbnail components and layout for them

--- src/pages/UserThemes.tsx
+++ src/pages/UserThemes.tsx
@@ -1,46 +1,27 @@
 import {
   AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
   Box,
-  Paper,
   Button,
   CssBaseline,
   Grid,
+  IconButton,
+  Paper,
+  Toolbar,
+  Typography,
 } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import React, { useState } from 'react';
-import LightModeIcon from '@mui/icons-material/LightMode';
+import React, { useContext, useState } from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import { FirebaseContext } from 'components/FirebaseProvider/FirebaseProvider';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
+import CurrentTheme from 'components/Themes/ThemeThumbnail';
+import ThemeCreator from 'components/Themes/ThemeCreator';
 
 enum Colors {
-  primary = '#bf9c0f',
-  secondary = '#f22230',
+  primary = '#f5a506',
+  secondary = '#8f1be3',
 }
-
-// const customTheme = createTheme({
-//   palette: {
-//     primary: {
-//       main: '#61dafb',
-//     },
-//     secondary: {
-//       main: '#da61bf',
-//     },
-//   },
-// });
-
-// const defaultTheme = createTheme({
-//   palette: {
-//     primary: {
-//       main: '#db921d',
-//     },
-//     secondary: {
-//       main: '#e647ec',
-//     },
-//   },
-// });
 
 const lightMode = createTheme({
   palette: {
@@ -67,8 +48,22 @@ const darkMode = createTheme({
 });
 
 const TestPage = () => {
+  const { firestore } = useContext(FirebaseContext);
   const [curTheme, setTheme] = useState(lightMode);
   const [isLightTheme, setIsLightTheme] = useState(true);
+  const [isCreating, setIsCreating] = useState(false);
+
+  const addTheme = async () => {
+    try {
+      const docTheme = await addDoc(collection(firestore, 'themes'), {
+        name: 'name',
+        primary: 'primaryColor',
+        secondary: 'secondaryColor',
+      });
+    } catch (error) {
+      console.log('Error adding document: ', error);
+    }
+  };
 
   const handleChangeTheme = () => {
     if (isLightTheme) {
@@ -83,99 +78,81 @@ const TestPage = () => {
   return (
     <ThemeProvider theme={curTheme}>
       <CssBaseline />
-      <Box>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="span" sx={{ flexGrow: 1 }}>
-              Logo
-            </Typography>
-            <IconButton color="inherit" onClick={handleChangeTheme}>
-              {isLightTheme ? <DarkModeIcon /> : <LightModeIcon />}
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Paper
-          elevation={24}
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItem: 'center',
-            maxWidth: '1000px',
-            width: '100%',
-            height: '500px',
-            m: '50px auto',
-            p: '20px',
-          }}
-        >
-          <Typography variant="h3" sx={{ alignSelf: 'center', mt: '2rem' }}>
-            My themes
-          </Typography>
-          <Button variant="contained" sx={{ m: '2rem auto' }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton color="inherit" onClick={handleChangeTheme}>
+            {isLightTheme ? <DarkModeIcon /> : <LightModeIcon />}
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Box
+        sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', m: '0 20px 20px' }}
+      >
+        <Typography variant="h3" sx={{ m: '1rem' }}>
+          Available themes
+        </Typography>
+        {isCreating ? (
+          <ThemeCreator setIsCreating={setIsCreating} />
+        ) : (
+          <Button variant="contained" sx={{ m: '1rem' }} onClick={() => setIsCreating(true)}>
             Create new theme
           </Button>
-          <Typography variant="h5" sx={{ alignSelf: 'center', mb: '2rem' }}>
-            Available themes
-          </Typography>
-          <Grid container spacing={0} columns={4}>
-            <Grid xs={1}>
-              <Paper
-                sx={{
-                  height: '150px',
-                  background: 'linear-gradient(180deg, #3f51b5 50%, #f50057 50%)',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                <Button variant="contained">Apply</Button>
-              </Paper>
-            </Grid>
-            <Grid xs={1}>
-              <Paper
-                sx={{
-                  height: '150px',
-                  background: 'linear-gradient(180deg, #e84848 50%, #47e4ec 50%)',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                <Button variant="contained">Apply</Button>
-              </Paper>
-            </Grid>
-            <Grid xs={1}>
-              <Paper
-                sx={{
-                  height: '150px',
-                  background:
-                    'linear-gradient(180deg, rgba(237,204,32,1) 50%, rgba(138,208,22,1) 50%)',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                <Button variant="contained">Apply</Button>
-              </Paper>
-            </Grid>
-            <Grid xs={1}>
-              <Paper
-                sx={{
-                  height: '150px',
-                  background: 'linear-gradient(180deg, #a918d9 50%, rgba(138,208,22,1) 50%)',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                <Button variant="contained">Apply</Button>
-              </Paper>
-            </Grid>
+        )}
+        <Grid container spacing={0} columns={4}>
+          <Grid xs={1}>
+            <Paper
+              sx={{
+                height: '150px',
+                background: 'linear-gradient(180deg, #3f51b5 50%, #f50057 50%)',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Button variant="contained">Apply</Button>
+            </Paper>
           </Grid>
-        </Paper>
-        <Box sx={{ height: '300px' }}></Box>
+          <Grid xs={1}>
+            <Paper
+              sx={{
+                height: '150px',
+                background: 'linear-gradient(180deg, #d91818 50%, #12a71e 50%)',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Button variant="contained">Apply</Button>
+            </Paper>
+          </Grid>
+          <Grid xs={1}>
+            <Paper
+              sx={{
+                height: '150px',
+                background:
+                  'linear-gradient(180deg, rgba(237,204,32,1) 50%, rgba(138,208,22,1) 50%)',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Button variant="contained">Apply</Button>
+            </Paper>
+          </Grid>
+          <Grid xs={1}>
+            <Paper
+              sx={{
+                height: '150px',
+                background: 'linear-gradient(180deg, #a918d9 50%, #12a6a7 50%)',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Button variant="contained">Apply</Button>
+            </Paper>
+          </Grid>
+        </Grid>
       </Box>
     </ThemeProvider>
   );
