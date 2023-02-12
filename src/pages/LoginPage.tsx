c3ca9c233d02cab3763e6077acc8de6a86834811
Grinmak <grinmak@gmail.com>
2023-02-12 22:27:16 +0300

    feat: complete logout function and add sign up page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-// import styled from '@emotion/styled';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import { FirebaseContext } from 'components/FirebaseProvider/FirebaseProvider';
-import { useSignInWithGoogle, useSignInWithGithub } from 'react-firebase-hooks/auth';
-import { Box, FormControl, Link, TextField } from '@mui/material';
+import {
+  useSignInWithGoogle,
+  useSignInWithEmailAndPassword,
+  useAuthState,
+} from 'react-firebase-hooks/auth';
+import { Box, TextField } from '@mui/material';
 import Typography from '@mui/material/Typography';
-// import SignUpPage from './SignUpPage';
-// import { FormWrapper } from 'components/common/FormWrapper';
-// import { InputUnstyled } from '@mui/base';
-import styled from '@emotion/styled';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { AppRoutes } from 'enum/AppRoutes';
+import GoogleIcon from '@mui/icons-material/Google';
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const auth = React.useContext(FirebaseContext).auth;
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
   const [signInWithGoogle] = useSignInWithGoogle(auth);
-  const [signInWithGitHub] = useSignInWithGithub(auth);
-  // const goHome = () => {
-  //   navigate('/');
-  // };
+
+  const [user] = useAuthState(auth);
+
+  React.useEffect(() => {
+    if (user) navigate(AppRoutes.Boards);
+  }, [user]);
 
   return (
     <Grid
@@ -30,32 +36,45 @@ const LoginPage = () => {
       <Grid
         p={5}
         sx={{
-          width: '400px',
           boxShadow: '4px 4px 4px 4px gray',
           textAlign: 'center',
           direction: 'column',
+          borderRadius: '15px',
         }}
       >
-        <Box component="form" sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+        <Box
+          component="form"
+          sx={{ width: '250px', display: 'flex', flexDirection: 'column', gap: '10px' }}
+        >
           <Typography>Login in to your account</Typography>
-          {/* <di2v> */}
-          <TextField label="Enter e-mail adress" type="email" />
-          <Button type="submit" variant="contained">
-            Continue
+          <TextField
+            label="E-mail adress"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            label="Password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            variant="contained"
+            // value=" Continue"
+            onClick={() => signInWithEmailAndPassword(email, password)}
+          >
+            Enter
           </Button>
-          {/* </div> */}
         </Box>
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
           or
           <Button variant="outlined" onClick={() => signInWithGoogle()}>
-            Google
-          </Button>
-          <Button variant="outlined" onClick={() => signInWithGitHub()}>
-            GitHub
+            <GoogleIcon sx={{ marginRight: '15px' }}></GoogleIcon>
+            Login with Google
           </Button>
         </Box>
-        <Typography>
-          Does`t have an account? <Link href={AppRoutes.SignUpPage}>SinUp for free!</Link>
+        <Typography sx={{ marginTop: '15px' }}>
+          Do not have an account? <br />
+          <Link to={AppRoutes.SignUpPage}> SinUp for free!</Link>
         </Typography>
       </Grid>
     </Grid>
