58326b82a51a18c598ae087d7805f39beee824bd
Anna Yemelyanova <devildriver2310@gmail.com>
2023-02-13 23:22:47 +0300

    feat: add user db record to the context

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { FirebaseContext } from 'components/FirebaseProvider/FirebaseProvider';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { AppRoutes } from 'enum/AppRoutes';
 import CircularProgress from '@mui/material/CircularProgress/CircularProgress';
+import { IUserItem } from 'types/User';
+import { doc, getDoc } from 'firebase/firestore';
+import { Collections } from 'enum/Collection';
+
+interface IUserContext {
+  user: IUserItem;
+}
+
+export const UserContext = createContext<IUserContext>({
+  user: {} as IUserItem,
+});
 
 const RequireAuth = () => {
   const navigate = useNavigate();
-  const { auth } = React.useContext(FirebaseContext);
+  const { auth, firestore } = useContext(FirebaseContext);
   const [user, loading] = useAuthState(auth);
-  React.useEffect(() => {
-    if (!user) {
+  const [userRecord, setUserRecord] = useState<IUserItem | null>(null);
+
+  const getUserRecord = async () => {
+    if (user) {
+      const record = await getDoc(doc(firestore, Collections.Users, user.uid));
+
+      setUserRecord({ ...record.data(), id: user.uid } as IUserItem);
+    }
+  };
+
+  useEffect(() => {
+    if (!user && !loading) {
       navigate(AppRoutes.LoginPage);
+    } else {
+      getUserRecord();
     }
   }, [user]);
 
-  if (loading) {
+  if (loading || !userRecord) {
     return <CircularProgress />;
   }
-  return <Outlet />;
+
+  return (
+    <UserContext.Provider
+      value={{
+        user: userRecord,
+      }}
+    >
+      <Outlet />
+    </UserContext.Provider>
+  );
 };
 
 export default RequireAuth;
