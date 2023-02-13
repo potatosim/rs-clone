2ff9a3b79fedcd1b1c5e75fd115e26a92717698a
Anna Yemelyanova <devildriver2310@gmail.com>
2023-02-13 23:22:47 +0300

    feat: add user db record to the context

--- src/pages/BoardsPage/BoardsPage.tsx
+++ src/pages/BoardsPage/BoardsPage.tsx
@@ -1,12 +1,15 @@
 import { Box, CircularProgress } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 
 import BoardCard from 'components/BoardCard';
 import CreateBoardButton from 'components/CreateBoardButton';
 import CreateBoardForm from 'components/CreateBoardForm';
 import { useBoards } from 'hooks/boardHooks/useBoards';
+import { UserContext } from 'components/RequireAuth';
 
 const BoardsPage = () => {
+  const { user } = useContext(UserContext);
+  console.log(user);
   const { boards, loading, handleDeleteBoard, handleRenameBoard } = useBoards();
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
