26ce159ee0e56465c5e809707cd37f8adf657db6
Leontiy Knyazev <leontiy.knyazev@gmail.com>
2023-02-18 23:47:59 +0600

    feat: implement theme loading

--- src/components/FirebaseProvider/FirebaseProvider.tsx
+++ src/components/FirebaseProvider/FirebaseProvider.tsx
@@ -3,11 +3,12 @@ import './firebase';
 import { Auth, getAuth } from 'firebase/auth';
 import { doc, Firestore, getFirestore } from 'firebase/firestore';
 import { FirebaseStorage, getStorage } from 'firebase/storage';
-import { FC, ReactElement, ReactNode, createContext } from 'react';
+import { FC, ReactElement, ReactNode, createContext, useState, useEffect } from 'react';
 import { IUserItem } from 'types/User';
 import { Collections } from 'enum/Collection';
 import { usersConverter } from 'helpers/converters';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
+import ModalLoader from 'components/common/ModalLoader';
 
 const auth = getAuth();
 const firestore = getFirestore();
@@ -32,15 +33,29 @@ interface FirebaseProviderProps {
 }
 
 const FirebaseProvider: FC<FirebaseProviderProps> = ({ children }) => {
-  const [user] = useDocumentData<IUserItem>(
-    doc(firestore, Collections.Users, auth?.currentUser?.uid || 'userId').withConverter(
-      usersConverter,
-    ),
+  const [userId, setUserId] = useState('userId');
+
+  const [user, loader] = useDocumentData<IUserItem>(
+    doc(firestore, Collections.Users, userId).withConverter(usersConverter),
   );
+
+  useEffect(() => {
+    auth.onAuthStateChanged((usr) => {
+      if (usr) {
+        setUserId(usr.uid);
+      } else {
+        setUserId('userId');
+      }
+    });
+  }, []);
+
+  if (loader) {
+    return <ModalLoader isOpen={loader} />;
+  }
 
   return (
     <FirebaseContext.Provider value={{ auth, firestore, storage, user: user || null }}>
-      {children}
+      {children} 
     </FirebaseContext.Provider>
   );
 };
