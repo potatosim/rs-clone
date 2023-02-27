b81dc793745fd093e211f9cbb014a561b4a25aa1
Anna Yemelyanova <devildriver2310@gmail.com>
2023-02-27 23:26:58 +0300

    fix: add link to rs-school in footer, delete comments

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -4,6 +4,7 @@ import RsLogo from 'static/images/rss.svg';
 import styled from '@emotion/styled';
 import PopUpGitHub from 'components/PopUpGitHub';
 import { useTheme } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 
 const FooterWrapper = styled(Box)(() => ({
   display: 'flex',
@@ -18,7 +19,10 @@ const Footer = () => {
   return (
     <FooterWrapper>
       <Toolbar sx={{ width: '100%', justifyContent: 'space-between', alignContent: 'center' }}>
-        <StyledLogo mode={theme.palette.mode} src={RsLogo} />
+        <Box component={Link} target="_blank" to="https://rs.school/js/">
+          <StyledLogo mode={theme.palette.mode} src={RsLogo} />
+        </Box>
+        Created in 2023
         <PopUpGitHub />
       </Toolbar>
     </FooterWrapper>
